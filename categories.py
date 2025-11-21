26b7a2259c75585b104898d0ea59b5201e16e7fc
Ksenya <moch.xe@gmail.com>
2025-11-21 20:39:39 +0300

    Revert "feature changed"
    This reverts commit 8ae8844e4cd6f0ead2bca6dae792c89a613563af.
    
    reverting changes

--- categories.py
+++ categories.py
@@ -11,4 +11,4 @@ class CategoryManager:
         for i, category in enumerate(self.categories, 1):
             print(f"{i}, {category}")
 
-"""Some changes in feature"""
+"""Some changes in main"""
